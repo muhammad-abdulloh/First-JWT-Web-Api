6ff91fc8829e0bf67ac8b12bc94f147efed090e4
Clever Coder <clevercoderr@gmail.com>
2022-04-06 08:08:50 +0500

    Add project files.

---
+++ JWTWebApiTest/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTWebApiTest
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ JWTWebApiTest/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTWebApiTest
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
